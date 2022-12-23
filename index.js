5f41367173181bfbf03790e653effc392ed1a7a2
Tamerlan Aliyev <tamerlan-aliyev-2021@mail.ru>
2022-12-23 17:34:41 +0400

    tamerlan finish task1

--- index.js
+++ index.js
@@ -1,79 +1,6 @@
 "use strict";
-// enum Gender {
-//   Male = "Male",
-//   Female = "Female",
-//   Other = "Other",
-// }
-// const genderName = {
-//   "1": Gender.Male,
-//   "2": Gender.Female,
-//   "3": Gender.Other,
-// };
-// const userName: string | null = prompt("Enter username : (example: Name)");
-// const surName: string | null = prompt("Enter surname : (example: Surname)");
-// const birthday: string | null = prompt(
-//   "Enter birthday : (example: DD/MM/YYYY)"
-// );
-// const gender: string | null = prompt(
-//   "Select gender: \n1.Male \n2.Female \n3.Other \n(example: 1)"
-// );
-// const password: string | null = prompt("Enter password : (example: password)");
-// const getUserAge = (): number => {
-//   if (birthday) {
-//     try {
-//       const newBirthday = birthday.split("/").reverse().join("-"); // Change input data to "YYYY-MM-DD"
-//       const date = new Date(newBirthday); // Find birthday date
-//       const ageDifMs = Date.now() - date.getTime(); // Compare current date and get different with ms
-//       const ageDate = new Date(ageDifMs); // get ms date
-//       return Math.abs(ageDate.getUTCFullYear() - 1970); // Find age
-//     } catch (error) {
-//       return 0;
-//     }
-//   }
-//   return 0;
-// };
-// const age = getUserAge();
-// const isCreate = (): boolean => {
-//   return age > 14 ? true : false;
-// };
-// const selectGender = () => {
-//   switch (gender) {
-//     case "1":
-//       return genderName[gender];
-//     case "2":
-//       return genderName[gender];
-//     case "3":
-//       return genderName[gender];
-//     default:
-//       return "Unknown";
-//   }
-// };
-// type User = {
-//   name: string;
-//   surname: string;
-//   age: number;
-//   gender: Gender | "Unknown";
-//   isEligible: boolean;
-// };
-// const createUser = (): { data?: User; msg: string } => {
-//   if (userName && surName && birthday && gender && password && age) {
-//     return {
-//       data: {
-//         name: userName,
-//         surname: surName,
-//         age: age,
-//         gender: selectGender(),
-//         isEligible: isCreate(),
-//       },
-//       msg: "User created successfully",
-//     };
-//   }
-//   return { msg: "User can't created" };
-// };
-// const { data, msg } = createUser();
-// data ? console.log(msg, data) : console.log(msg);
-//Bonus Task
-const userNameInput = document.getElementById("name");
+
+const uInput = document.getElementById("name");
 const surNameInput = document.getElementById("surname");
 const birthdayInput = document.getElementById("birthday");
 const genderInput = document.getElementById("gender");
@@ -86,7 +13,7 @@ const state = {
     gender: "",
     password: "",
 };
-userNameInput.onchange = (e) => {
+uInput.onchange = (e) => {
     const target = e.target;
     state.userName = target.value;
 };
